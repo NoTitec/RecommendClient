3bf0c6443f716b0d6b6226f62386b1b9881ce75b
NoTitec <zkxkr0@gmail.com>
2022-05-07 23:18:50 +0900

    버튼 ui 스레드추가

--- src/main/java/com/example/recommendclient/RecomendRecipeController.java
+++ src/main/java/com/example/recommendclient/RecomendRecipeController.java
@@ -1,5 +1,6 @@
 package com.example.recommendclient;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -149,61 +150,69 @@ public class RecomendRecipeController implements Initializable {
     }
     //----------------- 이밑은 이밴트 핸들러 메소드
     public void handleresetbtnAction(ActionEvent event){
-        System.out.println("새로고침버튼클릭");
-        //서버에게 요리목록 패킷요청
-        proto=new Protocol(0,0);
+        Thread thread=new Thread(){//버튼 눌렀을때 통신과 ui변경담당 스레드
+            @Override
+            public void run() {
+                System.out.println("새로고침버튼클릭");
+                //서버에게 요리목록 패킷요청
+                proto=new Protocol(0,0);
 
-        try {
-            bos.write(proto.getPacket());
-            bos.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                try {
+                    bos.write(proto.getPacket());
+                    bos.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-        //요리목록패킷 받아서 사진과 이미지 변경 , 받는패킷의 목록순서 날씨음식1,2 계절음식 1,2
-        try {
-            System.out.println(bis.read(buf));//수신버퍼 읽기시도후 실제 읽은 바이트수 출력하고 buf배열에 읽은것 저장
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int type=buf[0]; //타입
-        int code=buf[1]; //코드
-        pos=2;
-        for(int i=0;i<4;i++){//전송받은 요리 4개 추출해 저장
-            //1개요리 이름길이 추출
-            int onerecipenamelength=proto.byteArrayToInt(Arrays.copyOfRange(buf,pos,pos+4));
-            pos+=4;
-            //1개요리 이름길이만큼 읽어서 추출후 string 변환하여 요리이름배열에저장
-            byte[] name = Arrays.copyOfRange(buf, pos, pos+onerecipenamelength);//추출한길이만큼읽어 코드추출
-            recommendname[i] = new String(name);//추출 이름 String 변환해 저장
-            pos+=onerecipenamelength;
-            //1개요리 이미지 url 길이 추출
-            int onerecipeurllength=proto.byteArrayToInt(Arrays.copyOfRange(buf,pos,pos+4));
-            pos+=4;
-            //1개요리 이미지 url길이만큼 읽어서 추출후 string 변환하여 요리이미지배열에 저장
-            byte[] url = Arrays.copyOfRange(buf, pos, pos+onerecipeurllength);//추출한길이만큼읽어 코드추출
-            Imageurl[i]= new String(url);//추출 word를  string으로 변환하여 저장
-            pos+=onerecipenamelength;
-        }
-        //받은 요리의 이름과 이미지 url이용해 ui 정보 변경
-        wetherrecommend1.setText(recommendname[0]);
-        wetherImage1.setImage(new Image(Imageurl[0]));
-        wetherrecommend2.setText(recommendname[1]);
-        wetherImage2.setImage(new Image(Imageurl[1]));
-        seasonrecommend1.setText(recommendname[2]);
-        seasonImage1.setImage(new Image(Imageurl[2]));
-        seasonrecommend2.setText(recommendname[3]);
-        seasonImage2.setImage(new Image(Imageurl[3]));
-        //수신버퍼 읽을것 없을때까지 읽어서 버퍼비우기(1번째 통신이후 수신버퍼 읽을때 쓰레기값 방지위해)
-        while(true){
-            try {
-                if (bis.read()==-1) break;
-            } catch (IOException e) {
-                e.printStackTrace();
+                //요리목록패킷 받아서 사진과 이미지 변경 , 받는패킷의 목록순서 날씨음식1,2 계절음식 1,2
+                try {
+                    System.out.println(bis.read(buf));//수신버퍼 읽기시도후 실제 읽은 바이트수 출력하고 buf배열에 읽은것 저장
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                int type=buf[0]; //타입
+                int code=buf[1]; //코드
+                pos=2;
+                for(int i=0;i<4;i++){//전송받은 요리 4개 추출해 저장
+                    //1개요리 이름길이 추출
+                    int onerecipenamelength=proto.byteArrayToInt(Arrays.copyOfRange(buf,pos,pos+4));
+                    pos+=4;
+                    //1개요리 이름길이만큼 읽어서 추출후 string 변환하여 요리이름배열에저장
+                    byte[] name = Arrays.copyOfRange(buf, pos, pos+onerecipenamelength);//추출한길이만큼읽어 코드추출
+                    recommendname[i] = new String(name);//추출 이름 String 변환해 저장
+                    pos+=onerecipenamelength;
+                    //1개요리 이미지 url 길이 추출
+                    int onerecipeurllength=proto.byteArrayToInt(Arrays.copyOfRange(buf,pos,pos+4));
+                    pos+=4;
+                    //1개요리 이미지 url길이만큼 읽어서 추출후 string 변환하여 요리이미지배열에 저장
+                    byte[] url = Arrays.copyOfRange(buf, pos, pos+onerecipeurllength);//추출한길이만큼읽어 코드추출
+                    Imageurl[i]= new String(url);//추출 word를  string으로 변환하여 저장
+                    pos+=onerecipenamelength;
+                }
+                //받은 요리의 이름과 이미지 url이용해 ui 정보 변경
+                Platform.runLater(()->wetherrecommend1.setText(recommendname[0]));
+                Platform.runLater(()->wetherImage1.setImage(new Image(Imageurl[0])));
+                Platform.runLater(()->wetherrecommend2.setText(recommendname[1]));
+                Platform.runLater(()->wetherImage2.setImage(new Image(Imageurl[1])));
+                Platform.runLater(()->seasonrecommend1.setText(recommendname[2]));
+                Platform.runLater(()->seasonImage1.setImage(new Image(Imageurl[2])));
+                Platform.runLater(()->seasonrecommend2.setText(recommendname[3]));
+                Platform.runLater(()->seasonImage2.setImage(new Image(Imageurl[3])));
+                //수신버퍼 읽을것 없을때까지 읽어서 버퍼비우기(1번째 통신이후 수신버퍼 읽을때 쓰레기값 방지위해)
+                while(true){
+                    try {
+                        if (bis.read()==-1) break;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                pos=2;
+                //wetherImage1.setImage(new Image("https://recipe1.ezmember.co.kr/cache/recipe/2022/02/16/8e34b759f6386912756c9a0f9d2255f91.jpg"));
             }
 
-        }
-        pos=2;
-        //wetherImage1.setImage(new Image("https://recipe1.ezmember.co.kr/cache/recipe/2022/02/16/8e34b759f6386912756c9a0f9d2255f91.jpg"));
+        };
+        thread.setDaemon(true);
+        thread.start();
     }
 }
